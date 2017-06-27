caa680cb1db7c279e3a869d348c10ea982df51f5
Venkat <vanakkamvenkat@zoho.com>
2017-06-27 22:02:04 +0530

    Testing TortoiseGIT functionality

--- BuiltInFunctions.java
+++ BuiltInFunctions.java
@@ -7,6 +7,7 @@ public class BuiltInFunctions {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Creating a sample array for testing print functionality
 		String [] sample = {"Juve","Barca","Iniesta"};
 		String str= Arrays.toString(sample);
 		System.out.println(str);
